0636cd731315e2a656a554d130bf2fedad07df1f
Tom Leitner <tom.leitner@deutschebahn.com>
2022-06-30 16:27:26 +0200

    keep_on_coding_dynamic_programming

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -1,12 +1,43 @@
 
-def fib(n):
-    if (n <= 2):
-        return 1
+# Fibonacci
+
+# def fib(n):
+#     if (n <= 2):
+#         return 1   
+#     return fib(n - 1) + fib(n -2)
+# print(fib(5))
+
+# def dib(n):
+#     if (n<=1):
+#         return
+#     dib(n-1)
+#     dib(n-1)
+
+# print(dib(5))
+
+# memoization
+
+def memoize(f):
+    memo = {}
+    def helper(x):
+        if x not in memo:            
+            memo[x] = f(x)
+        return memo[x]
+    return helper
     
-    return fib(n - 1) + fib(n -2)
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+fib = memoize(fib)
+
+print(fib(40))
 
 
-print(fib(5))
 
-
-https://www.youtube.com/watch?v=oBt53YbR9Kk
+# https://www.youtube.com/watch?v=oBt53YbR9Kk
